06cd2d90b70860aeb9a5521dfac02c93c0c8fee7
ksheer <ksheer506@naver.com>
2023-10-07 12:44:04 +0000

    10-3 solved

--- projects/generics/hidash/03-pick/index.ts
+++ projects/generics/hidash/03-pick/index.ts
@@ -1,2 +1,15 @@
 // Write your pick function here! ✨
 // You'll need to export it so the tests can run it.
+export const pick = <T extends Record<string, unknown>, S extends keyof T>(
+	container: T,
+	key: S
+) => container[key];
+
+pick({ a: 1, b: "2" }, "a");
+/* 
+<T extends Record<string, unknown>>(container: T, key: keyof T) => {}이면 
+return 타입이 string | number
+
+<T extends Record<string, unknown>, S extends keyof T>(container: T, key: S) => {}이면
+return 타입이 number
+*/
