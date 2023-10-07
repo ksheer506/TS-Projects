bd6148003643c8c0116170359458b34fdc9c5de1
ksheer <ksheer506@naver.com>
2023-10-07 12:44:28 +0000

    10-4 solved

--- projects/generics/hidash/04-pickmany/index.ts
+++ projects/generics/hidash/04-pickmany/index.ts
@@ -1,2 +1,19 @@
 // Write your pickMany function here! ✨
 // You'll need to export it so the tests can run it.
+export const pickMany = <T extends Record<string, unknown>, S extends keyof T>(
+	container: T,
+	keys: S[]
+) => keys.map((k) => container[k]);
+
+pickMany({ a: 1, b: false }, ["b"]);
+/* 
+<T extends Record<string, unknown>, S extends ReadonlyArray<keyof T>>(
+  container: T,
+  keys: S
+) => {}이면 return 타입이 (number | string)[]
+
+<T extends Record<string, unknown>, S extends keyof T>(
+  container: T,
+  keys: S[]
+) => {}이면 return 타입이 string[]
+*/
