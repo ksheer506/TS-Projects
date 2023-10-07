2c0be2962bcbdd2448ddd575d3a7d92508056c87
ksheer <ksheer506@naver.com>
2023-10-07 12:43:33 +0000

    10-1 solved

--- projects/generics/hidash/01-unique/index.ts
+++ projects/generics/hidash/01-unique/index.ts
@@ -1,2 +1,5 @@
 // Write your unique function here! ✨
 // You'll need to export it so the tests can run it.
+export const unique = <T>(...items: T[][]) => {
+	return [...new Set(items.flatMap((e) => e))];
+};
