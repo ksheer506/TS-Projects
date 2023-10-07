a6f3c7800b86bb99227e7cbcb1a6e7c0e639504c
ksheer <ksheer506@naver.com>
2023-10-07 12:43:48 +0000

    10-2 solved

--- projects/generics/hidash/02-zip/index.ts
+++ projects/generics/hidash/02-zip/index.ts
@@ -1,2 +1,16 @@
 // Write your zip function here! ✨
 // You'll need to export it so the tests can run it.
+export const zip = <T, S>(a: T[], b: S[]) => {
+	const longestArr = a.length < b.length ? b : a;
+	const initialValue: (T | S)[] = [];
+
+	return longestArr.reduce((p, _, i) => {
+		if (a[i] !== undefined) {
+			p.push(a[i]);
+		}
+		if (b[i] !== undefined) {
+			p.push(b[i]);
+		}
+		return p;
+	}, initialValue);
+};
